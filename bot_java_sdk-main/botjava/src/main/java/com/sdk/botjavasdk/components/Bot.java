b09bf856239d1d107fc40eb2e2fc30c4503a6d70
gaohaojie1477 <1482099959@qq.com>
2023-07-18 15:25:15 +0800

    新增pub_key

--- bot_java_sdk-main/botjava/src/main/java/com/sdk/botjavasdk/components/Bot.java
+++ bot_java_sdk-main/botjava/src/main/java/com/sdk/botjavasdk/components/Bot.java
@@ -11,14 +11,22 @@ public class Bot {
     private static String bot_secret=""; //机器人secret
     private static String pub_key="";//机器人的pub_key
 
+    private static String bot_secret1=Cipher.hmacSha256(pub_key,bot_secret);//加密后的secret
     public static class Bots{
         public static String getBot_id() {return bot_id;}
-        public static String getBot_secret() {
-            return bot_secret;
+        public static String getBot_secret() {return bot_secret1;}
+        //未加密的secret
+        public static String getBot_secretyuan(){return bot_secret;}
+        public static String getBot_pub_key(){return pub_key;}
+        public static void init(JSONObject params){
+            Event param = JSONObject.parseObject(params.toJSONString(), Event.class);
+            EventClass.Main.main(param.getEvent());
         }
-        public static void init(Event params){
-            EventClass.Main.main(params.getEvent());
+        //签名校验
+        public static Boolean verification(String sign,JSONObject params)throws Exception{
+            return VerificationHelper.verify(sign,params.toJSONString(),bot_secret,pub_key);
         }
+
         public static String text(){
             return EventClass.Main.data_text();
         }
